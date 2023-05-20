a8818d33af4ff706d03c032c78579051e3de1ce7
Abu Bakar <abubakar.hasan.cse@gmail.com>
2023-05-20 15:20:11 +0600

    Error resolved

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 require("dotenv").config()
 const port = process.env.PORT || 5000;
-
-const data = require("./data/Toys.json")
-
-/**
- * Ge1Wt4Jz0FS22Rje
- * ToyQuestDb12
- */
 
 // middleware
 app.use(cors());
@@ -20,46 +12,7 @@ app.use(express.json());
 app.get('/', (req, res) =>{
     res.send(data);
 });
-/**
- * ---------------------------------
- *MongoDB Connection Activity Start
- *  _________________________________
- */
-const uri = `mongodb+srv://${process.env.UsEr_db}:${process.env.PaSs_db}@cluster0.qibhtxb.mongodb.net/?retryWrites=true&w=majority`;
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-     client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
-}
-run().catch(console.dir);
-
-
-
-
-
-
-/**
- * ---------------------------------
- *MongoDB Connection Activity End
- *  _________________________________
- */
 app.listen(port, () => {
   console.log(`Toy marketplace server is running on port: ${port}`)
 });
