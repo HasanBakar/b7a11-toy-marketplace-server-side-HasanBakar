df9f9f78cd0df4b4039fa1dadb037ec20bf74287
Abu Bakar <abubakar.hasan.cse@gmail.com>
2023-05-20 15:12:22 +0600

    env file deleted

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) =>{
  *MongoDB Connection Activity Start
  *  _________________________________
  */
-const uri = `mongodb+srv://${process.env.Db__UsEr}:${process.env.Db__PaSs}@cluster0.qibhtxb.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.UsEr_db}:${process.env.PaSs_db}@cluster0.qibhtxb.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -41,7 +41,7 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+     client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
